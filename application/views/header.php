5cc1aea7cbb31268b799cf595c7d40c9f6595806
pesder <j_h_liau@yahoo.com.tw>
2017-07-19 10:46:36 +0800

    加入基本頁首頁尾
    加入基本頁首頁尾

---
+++ application/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo config_item('web_title'); ?></title>
+    <!-- Bootstrap -->
+    <link href="<?php echo config_item('base_url'); ?>/plugin/css/bootstrap.min.css" rel="stylesheet">
+
+	<script src="<?php echo config_item('base_url'); ?>/plugin/js/bootstrap.min.js"></script>
+  </head>
+  <body>
