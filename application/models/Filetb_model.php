d367a81da17780962cbe7323976372222f69559f
pesder <j_h_liau@yahoo.com.tw>
2017-07-21 15:59:31 +0800

    當試檔案對應

---
+++ application/models/Filetb_model.php
@@ -0,0 +1,74 @@
+<?php
+
+class Filetb_model extends CI_Model {
+	
+		public function __construct()
+        {
+                // Call the CI_Model constructor
+                parent::__construct();
+
+                //連結資料庫
+                $this->load->database();
+        }
+
+        //查詢
+        public function query($id = 0) 
+        {
+
+        	$this->db->select('*');
+        	$this->db->from('filetb');
+        	if ($id > 0) 
+        	{
+        		$this->db->where('fid', $id);
+        	}
+        	$this->db->order_by('fid','desc');
+        	//$this->db->where();
+        	$query = $this->db->get();
+        	$date = $query->result();
+        	// 回傳
+        	if ($id > 0)
+        	{
+        		return $query->row();
+        	}
+        	else
+        	{
+        		return $query->result();
+        	}
+        }
+        //有限查詢
+        public function queryLimit($limit) 
+        {
+
+            $this->db->select('*');
+            $this->db->from('filetb');
+            $this->db->order_by('fid','desc');
+            $this->db->limit($limit);
+            $query = $this->db->get();
+            return $query->result();
+        }
+        //依條件查詢
+        public function queryBy($cd1, $cd2) 
+        {
+
+            $this->db->select('*');
+            $this->db->from('filetb');
+            $this->db->where($cd1, $cd2);
+            $this->db->order_by('fid','desc');
+            //$this->db->where();
+            $query = $this->db->get();
+            return $query->result();
+        }
+        //搜尋檔案名稱對應
+        public function mathFile($pid,$gid,$filename)
+        {
+            $this->db->select('*');
+            $this->db->from('filetb');
+            $this->db->like($filename);
+            $this->db->where('partid',$pid);
+            $this->db->where('userid',$uid);
+            $this->db->where('filelist',$filename);
+            $query = $this->db->get();
+            return $query->row();
+        }
+
+}
