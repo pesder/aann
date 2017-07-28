bb6f6f308aef110381baa5eaad1364c333d46f15
pesder <j_h_liau@yahoo.com.tw>
2017-07-28 09:27:45 +0800

    以 php escaping from html 方式完成附件下載判斷

--- application/views/main_viewann_file.php
+++ application/views/main_viewann_file.php
@@ -1,16 +1,11 @@
         <?php foreach ($annfile as $index => $row) :?>
         <tr>
             <td colspan="6">
-            <?php
-                if ($filenotthere[$index] == 1)
-                {
-                    echo "<a href=\"" . config_item('base_url') . "/index.php/Main/download/" . $head->partid . "/" . $body->userid . "/" . $row . "\">★相關附件 " . $index + 1 . "：" . $annfilereadable[$index] . "</a>";
-                } elseif ($filenotthere[$index] == 0)
-                {
-                     echo "★相關附件 " . $index + 1 . "：" . $annfilereadable[$index];
-                }
-            ?>
+            <?php if ($filenotthere[$index] == 1):?>
             <a href="<?=config_item('base_url');?>/index.php/Main/download/<?=$head->partid?>/<?=$body->userid?>/<?php echo $row?>">★相關附件 <?php echo $index + 1?>：<?php echo $annfilereadable[$index]?></a>
+            <?php else:?>
+            ★相關附件 <?php echo $index + 1?>：<?php echo $annfilereadable[$index]?>
+            <?php endif;?>
             </td>
         </tr>        
         <?php endforeach; ?>
